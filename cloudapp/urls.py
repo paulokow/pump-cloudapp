20e2b7ee5fb7ff6ffe3af8ca2b237856028652db
paulokow <31203062+paulokow@users.noreply.github.com>
2017-10-03 09:07:34 +0200

    BUGFIX: redirect of empty url

--- cloudapp/urls.py
+++ cloudapp/urls.py
@@ -8,7 +8,7 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^bgmonitor/', include('bgmonitor.urls')),
-    url(r'^/$', RedirectView.as_view(url='/bgmonitor')),
-    url(r'^admin/', admin.site.urls)
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', RedirectView.as_view(url='/bgmonitor')),
 ]
 
