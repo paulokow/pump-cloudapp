cb9585bce7caba590d81614bc820e5dd8b829c36
paulokow <31203062+paulokow@users.noreply.github.com>
2017-12-20 14:01:05 +0100

    BUGFIX: static files should be served now

--- cloudapp/urls.py
+++ cloudapp/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 from django.views.generic import RedirectView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.autodiscover()
 
@@ -10,5 +11,5 @@ urlpatterns = [
     url(r'^bgmonitor/', include('bgmonitor.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^$', RedirectView.as_view(url='/bgmonitor')),
-]
+] + staticfiles_urlpatterns()
 
